485077689a138c5294e98fa37b924354b39fdae5
tkane421 <tkane421@pm.me>
2025-05-02 17:13:38 +0100

    Simplifying

--- pullCSVs.py
+++ pullCSVs.py
@@ -2,36 +2,36 @@ import requests
 from bs4 import BeautifulSoup
 
 main_leagues = {
-    'England': {'page': 'englandm.php', 'prefix' : 'E'},
-    'Scotland': {'page': 'scotlandm.php', 'prefix' : 'SC'},
-    'Germany': {'page': 'germanym.php', 'prefix' : 'D'},
-    'Italy': {'page': 'italym.php', 'prefix' : 'I'},
-    'Spain': {'page': 'spainm.php', 'prefix' : 'SP'},
-    'France': {'page': 'francem.php', 'prefix': 'F'},
-    'Netherlands': {'page': 'netherlandsm.php', 'prefix': 'N'},
-    'Belgium': {'page': 'belgiumm.php', 'prefix': 'B'},
-    'Portugal': {'page': 'portgualm.php', 'prefix': 'P'},
-    'Turkey': {'page': 'turkeym.php', 'prefix': 'T'},
-    'Greece': {'page': 'greecem.php', 'prefix': 'G'}
+    'england': ['E0','E1','E2','E3','EC'],
+    'scotland': ['SC0','SC1','SC2','SC3'],
+    'germany': ['D1', 'D2'],
+    'italy': ['I1', 'I2'],
+    'spain': ['SP1', 'SP2'],
+    'france': ['F1', 'F2'],
+    'netherlands': ['N1'],
+    'belgium': ['B1'],
+    'portugal': ['P1'],
+    'turkey': ['T1'],
+    'greece': ['G1']
 }
 
 extra_leagues = {
-    'Argentina': {'page': 'argentina.php', 'file': 'ARG'},
-    'Austria': {'page': 'austria.php', 'file': 'AUT'},
-    'Brazil': {'page': 'brazil.php', 'file': 'BRA'},
-    'China': {'page': 'china.php', 'file': 'CHN'},
-    'Denmark': {'page': 'denmark.php', 'file': 'DNK'},
-    'Finland': {'page': 'finland.php', 'file': 'FIN'},
-    'Ireland': {'page': 'ireland.php', 'file': 'IRL'},
-    'Japan': {'page': 'japan.php', 'file': 'JPN'},
-    'Mexico': {'page': 'mexico.php', 'file': 'MEX'},
-    'Norway': {'page': 'norway.php', 'file': 'NOR'},
-    'Poland': {'page': 'poland.php', 'file': 'POL'},
-    'Romania': {'page': 'romania.php', 'file': 'ROU'},
-    'Russia': {'page': 'russia.php', 'file': 'RUS'},
-    'Sweden': {'page': 'sweden.php', 'file': 'SWE'},
-    'Switzerland': {'page': 'switzerland.php', 'file': 'SWZ'},
-    'USA': {'page': 'usa.php', 'file': 'USA'}
+    'argentina': ['ARG'],
+    'austria': ['AUT'],
+    'brazil': ['BRA'],
+    'china': ['CHN'],
+    'denmark': ['DNK'],
+    'finland': ['FIN'],
+    'ireland': ['IRL'],
+    'japan': ['JPN'],
+    'mexico': ['MEX'],
+    'norway': ['NOR'],
+    'poland': ['POL'],
+    'romania': ['ROU'],
+    'russia': ['RUS'],
+    'sweden': ['SWE'],
+    'switzerland': ['SWZ'],
+    'usa': ['USA']
 }
 
 def scrape_last_updated(page_name):
@@ -49,12 +49,12 @@ def scrape_last_updated(page_name):
 
 for league in main_leagues:
     # Use the function to scrape the data for a specific URL
-    last_updated_text = scrape_last_updated(main_leagues[league]['page'])
+    last_updated_text = scrape_last_updated(league+"m.php")
     if last_updated_text is not None:
-        print(f"{league} {last_updated_text.lower()}")
+        print(f"{league.title()} {last_updated_text.lower()}")
 
 for league in extra_leagues:
     # Use the function to scrape the data for a specific URL
-    last_updated_text = scrape_last_updated(extra_leagues[league]['page'])
+    last_updated_text = scrape_last_updated(league+"php")
     if last_updated_text is not None:
-        print(f"{league} {last_updated_text.lower()}")
+        print(f"{league.title()} {last_updated_text.lower()}")
